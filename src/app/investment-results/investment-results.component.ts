cc828a3f03ae250185c12a1fd4fb65bbbc3ac5ae
Ubaldo-San-Juan <usan9fam@gmail.com>
2024-09-09 12:30:46 -0600

    InvestCalculator

---
+++ src/app/investment-results/investment-results.component.ts
@@ -0,0 +1,24 @@
+import { CurrencyPipe } from '@angular/common';
+import { Component, computed, inject} from '@angular/core';
+import { InvestmentService } from '../investment.service';
+
+@Component({
+  selector: 'app-investment-results',
+  templateUrl: './investment-results.component.html',
+  styleUrl: './investment-results.component.css'
+})
+export class InvestmentResultsComponent {
+
+  // @Input() results?: {
+  //   year: number,
+  //   interest: number,
+  //   valueEndOfYear: number,
+  //   annualInvestment: number,
+  //   totalInterest: number,
+  //   totalAmountInvested: number,
+  // }[];
+
+  private investmentService =  inject(InvestmentService);
+
+  results = computed(() => this.investmentService.resultData());
+}
